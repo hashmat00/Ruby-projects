c85938b1dc52df987d406f12354a988d05917820
hashmat MH <hashmat00@aol.com>
2016-04-25 23:17:18 +0000

    add users, profile links to nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+   
+   before_action :set_user, only: [:edit, :update, :show]
+   
+   
+   def index
+       @users = User.paginate(page: params[:page], per_page: 3)
+       
+    end
+   
+   
    def new
        @user = User.new
    end
@@ -16,11 +26,11 @@ class UsersController < ApplicationController
     
     
     def edit
-       @user = User.find(params[:id])
+       
     end
     
     def update
-      @user = User.find(params[:id])
+      
       if @user.update(user_params)
          flash[:success] = "Your account been successfully updated"
          redirect_to articles_path
@@ -31,19 +41,23 @@ class UsersController < ApplicationController
     
     
     def show
-       @user = User.find(params[:id])
+       
        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
     
-    def index
-       @users = User.paginate(page: params[:page], per_page: 3)
-       
-    end
+
     
     private
     def user_params
        params.require(:user).permit(:username, :email, :password)
     end
+    
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
+    
     
 end
