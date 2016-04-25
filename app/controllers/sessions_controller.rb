f0301f5eab4c949aaa25a88c76c8ebdd2e56469a
hashmat MH <hashmat00@aol.com>
2016-04-25 21:46:11 +0000

    add user login, logout session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+    
+    def new
+    
+    end
+    
+    
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully login"
+            redirect_to user_path(user)
+        else
+            flash.now[:danger] = "there was somethign wrong with your loign information"
+            render 'new'
+        end
+    end
+    
+    
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "you have logged out"
+        redirect_to root_path
+        
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+end
